338ffbe42ee160d9fb461708acb19c5cef012c8d
Lam Nhan <lamnhan92@gmail.com>
2018-12-26 13:20:13 +0700

    fix update

--- src/commands/update/update.ts
+++ src/commands/update/update.ts
@@ -12,39 +12,46 @@ interface Options {
 }
 
 export async function updateCommand(options: Options) {
+    if (! await hasUpdate()) {
+        console.log('\n Up to date :)');
+    } else {
+        if (!options.yes) {
+            logUpdateMessage();
+        } else {
+            console.log('New version available, start updating now.');
+            await exec('npm install -g @sheetbase/cli@latest');
+        }
+    }
+}
+
+export async function hasUpdate() {
     const { data: { latest } } = await axios({
         method: 'GET',
         url: `https://registry.npmjs.org/-/package/@sheetbase/cli/dist-tags`,
     });
+    return (latest !== version);
+}
 
-    if (latest === version) {
-        console.log('\n Up to date :)');
-    } else {
-        if (!options.yes) {
-            const table = ttyTable(
-                [{ value: 'Update available!', headerAlign: 'left', align: 'left' }],
-                [
-                    ['Sheetbase CLI got a new version, please update by:'],
-                    ['$ ' + magenta('npm install -g @sheetbase/cli@latest')],
-                ],
-                { compact: true },
-            );
-            console.log('\n');
-            console.log(table.render());
-            console.log('\n');
-        } else {
-            console.log('\nNew version available, start update now.');
-            await exec('npm install -g @sheetbase/cli@latest');
-        }
-    }
+export function logUpdateMessage() {
+    const table = ttyTable(
+        [{ value: 'Update available!', headerAlign: 'left', align: 'left' }],
+        [
+            ['Sheetbase CLI got a new version, please update by:'],
+            ['$ ' + magenta('npm install -g @sheetbase/cli@latest')],
+        ],
+        { compact: true },
+    );
+    console.log('\n');
+    console.log(table.render());
+    console.log('\n');
 }
 
 export async function checkUpdate() {
     const now = (new Date()).getTime();
     const latestCheck = configstore.get('latest_update_check');
     const beenHours = latestCheck ? Math.round((now - latestCheck) / 60000) : 24;
-    if (beenHours >= 24) {
+    if (beenHours >= 24 && await hasUpdate()) {
+        logUpdateMessage();
         configstore.set('latest_update_check', now);
-        await updateCommand({ yes: false });
     }
 }
