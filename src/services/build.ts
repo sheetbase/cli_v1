01f6912d066d0259809481f3de7c607f9022336a
Lam Nhan <lamnhan92@gmail.com>
2019-02-16 21:47:27 +0700

    update meta tags

--- src/services/build.ts
+++ src/services/build.ts
@@ -40,7 +40,7 @@ export function githubIndexHtmlSPAGenerator(html: string, base?: string) {
     );
     // change base
     if (!!base) {
-        html = replaceBetween(html, base, '<base href="', '"/>');
+        html = replaceBetween(html, base, '<base href="', '" />');
     }
     return html;
 }
@@ -82,30 +82,30 @@ export function prerenderer(
     if (!!title) {
         html = replaceBetween(html, title, [
             ['<title>', '</title>'],
-            ['<meta itemprop="name" content="', '"/>'], // google
-            ['<meta name="twitter:title" content="', '"/>'], // twitter
-            ['<meta name="og:title" content="', '"/>'], // facebook
+            ['<meta itemprop="name" content="', '" />'], // google
+            ['<meta name="twitter:title" content="', '" />'], // twitter
+            ['<meta name="og:title" content="', '" />'], // facebook
         ]);
     }
     if (!!url) {
         html = replaceBetween(html, url, [
-            ['<meta name="og:url" content="', '"/>'], // facebook
+            ['<meta name="og:url" content="', '" />'], // facebook
         ]);
     }
     if (!!description) {
         html = replaceBetween(html, description, [
-            ['<meta name="description" content="', '"/>'],
-            ['<meta itemprop="description" content="', '"/>'], // google
-            ['<meta name="twitter:description" content="', '"/>'], // twitter
-            ['<meta name="og:description" content="', '"/>'], // facebook
+            ['<meta name="description" content="', '" />'],
+            ['<meta itemprop="description" content="', '" />'], // google
+            ['<meta name="twitter:description" content="', '" />'], // twitter
+            ['<meta name="og:description" content="', '" />'], // facebook
         ]);
     }
     if (!!image) {
         html = replaceBetween(html, image, [
-            ['<meta name="image" content="', '"/>'],
-            ['<meta itemprop="image" content="', '"/>'], // google
-            ['<meta name="twitter:image:src" content="', '"/>'], // twitter
-            ['<meta name="og:image" content="', '"/>'], // facebook
+            ['<meta name="image" content="', '" />'],
+            ['<meta itemprop="image" content="', '" />'], // google
+            ['<meta name="twitter:image:src" content="', '" />'], // twitter
+            ['<meta name="og:image" content="', '" />'], // facebook
         ]);
     }
     return html;
