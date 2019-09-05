4abf51a0a03bc74346874cb9fadc124c2a1a75f2
Lam Nhan <lamnhan92@gmail.com>
2019-09-05 16:25:50 +0700

    default theme to starter

--- src/commands/project/project-start.ts
+++ src/commands/project/project-start.ts
@@ -86,13 +86,13 @@ export async function projectStartCommand(params: string[], options?: Options) {
 
 async function resolveResource(resource?: string) {
     /**
-     * n/a > blank@latest
+     * n/a > starter@latest
      * name > name@latest
      * name@ver > name@ver
      * <name>/<repo>@ver
      * full zip url > full zip url
      */
-    resource = (resource || 'sheetbase-themes/blank').replace('@latest', '');
+    resource = (resource || 'sheetbase-themes/starter').replace('@latest', '');
     if (!resource.endsWith('.git') && !resource.endsWith('.zip')) {
         // add repo org
         if (resource.indexOf('/') < 0) {
